fb33299ee62d0f28e69a3614bbfdb46d9c2974dc
Altaf Khan <altafkhan.nep.com>
2026-02-16 15:52:20 +0545

    recursion problem of premutation & combination

---
+++ Recursion Basic(part-2)/FrindsPairingProblem.java
@@ -0,0 +1,25 @@
+public class FrindsPairingProblem {
+    public static int pairingWays(int n){
+        // base case
+        if(n == 1 || n == 2){
+            return n;
+        }
+        // kaam
+        // return pairingWays(n - 1) + (n-1)*pairingWays(n-2);
+
+        // choice
+        // 1.single
+        int fm1 = pairingWays(n-1);
+        // 2.pair
+        int fm2 = pairingWays(n-2);
+        int pairWays = (n-1) * fm2;
+
+        // total ways 
+        int totWays = fm1 + pairWays;
+        return totWays;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("Total ways "+pairingWays(n));
+    }
+}
