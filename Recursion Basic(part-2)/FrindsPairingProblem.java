9cf920efb90552769ab6df7d1555975f637755fe
Altaf Khan <altafkhan.nep.com>
2026-02-16 17:09:01 +0545

    the recursive function problem

--- Recursion Basic(part-2)/FrindsPairingProblem.java
+++ Recursion Basic(part-2)/FrindsPairingProblem.java
@@ -19,7 +19,7 @@ public class FrindsPairingProblem {
         return totWays;
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n = 3;
         System.out.println("Total ways "+pairingWays(n));
     }
 }
