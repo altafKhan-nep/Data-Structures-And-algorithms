d01b8eeaf503183d883c58161c6838858bb733f4
Altaf Khan <altafkhan.nep.com>
2026-02-17 14:50:07 +0545

    In this problem we can convert number to string

---
+++ Recursion Basic(part-2)/Assingment/NumToString.java
@@ -0,0 +1,25 @@
+ package Assingment;
+
+public class NumToString {
+
+    static String digits[] = {"zero", "one", "two", "three", "four", "five", "six",
+"seven", "eight", "nine"};
+
+public static void printDigits(int num){
+
+    //  base case
+    if(num == 0){
+        return;
+    }
+    int lastDigit = num % 10;
+    printDigits(num/10);
+    System.out.print(digits[lastDigit] + " ");
+    
+    
+    
+}
+public static void main(String[] args) {
+    printDigits(1910);
+}
+    
+}
